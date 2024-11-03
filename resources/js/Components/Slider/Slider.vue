967ada80f6e449b74628dca078667af58bd7806b
Maxim Lugovskoy <77089911+mlugovskoy@users.noreply.github.com>
2024-11-03 14:35:20 +0900

    Main slider (#1)
    * feat: Работы по слайдеру
    
    * feat: Закончил верстку слайдера

---
+++ resources/js/Components/Slider/Slider.vue
@@ -0,0 +1,164 @@
+<script setup>
+
+import {Swiper, SwiperSlide} from "swiper/vue";
+import {ref} from "vue";
+import 'swiper/css';
+import 'swiper/css/navigation';
+import {Navigation} from "swiper/modules";
+
+const modules = ref([Navigation]);
+</script>
+
+<template>
+    <swiper
+        :slides-per-view="6"
+        :space-between="5"
+        :navigation="true"
+        :loop="true"
+        :modules="modules"
+    >
+        <swiper-slide>
+            <a href="#" class="transition-all hover:opacity-95">
+                <div class="h-3/4 relative">
+                    <img class="h-full w-full object-cover" src="/images/car.jpg" alt="car">
+                    <span
+                        class="absolute bottom-4 left-4 bg-emerald-400 px-2 rounded-xl text-white text-sm">4 500 000 Р</span>
+                </div>
+                <div class="h-1/4 px-2 flex flex-col justify-center">
+                    <h4 class="text-xl mb-0.5">BMW X8</h4>
+                    <span class="text-md">Благовещенск</span>
+                </div>
+            </a>
+        </swiper-slide>
+        <swiper-slide>
+            <a href="#" class="transition-all hover:opacity-95">
+                <div class="h-3/4 relative">
+                    <img class="h-full w-full object-cover" src="/images/car.jpg" alt="car">
+                    <span
+                        class="absolute bottom-4 left-4 bg-emerald-400 px-2 rounded-xl text-white text-sm">4 500 000 Р</span>
+                </div>
+                <div class="h-1/4 px-2 flex flex-col justify-center">
+                    <h4 class="text-xl mb-0.5">BMW X8</h4>
+                    <span class="text-md">Благовещенск</span>
+                </div>
+            </a>
+        </swiper-slide>
+        <swiper-slide>
+            <a href="#" class="transition-all hover:opacity-95">
+                <div class="h-3/4 relative">
+                    <img class="h-full w-full object-cover" src="/images/car.jpg" alt="car">
+                    <span
+                        class="absolute bottom-4 left-4 bg-emerald-400 px-2 rounded-xl text-white text-sm">4 500 000 Р</span>
+                </div>
+                <div class="h-1/4 px-2 flex flex-col justify-center">
+                    <h4 class="text-xl mb-0.5">BMW X8</h4>
+                    <span class="text-md">Благовещенск</span>
+                </div>
+            </a>
+        </swiper-slide>
+        <swiper-slide>
+            <a href="#" class="transition-all hover:opacity-95">
+                <div class="h-3/4 relative">
+                    <img class="h-full w-full object-cover" src="/images/car.jpg" alt="car">
+                    <span
+                        class="absolute bottom-4 left-4 bg-emerald-400 px-2 rounded-xl text-white text-sm">4 500 000 Р</span>
+                </div>
+                <div class="h-1/4 px-2 flex flex-col justify-center">
+                    <h4 class="text-xl mb-0.5">BMW X8</h4>
+                    <span class="text-md">Благовещенск</span>
+                </div>
+            </a>
+        </swiper-slide>
+        <swiper-slide>
+            <a href="#" class="transition-all hover:opacity-95">
+                <div class="h-3/4 relative">
+                    <img class="h-full w-full object-cover" src="/images/car.jpg" alt="car">
+                    <span
+                        class="absolute bottom-4 left-4 bg-emerald-400 px-2 rounded-xl text-white text-sm">4 500 000 Р</span>
+                </div>
+                <div class="h-1/4 px-2 flex flex-col justify-center">
+                    <h4 class="text-xl mb-0.5">BMW X8</h4>
+                    <span class="text-md">Благовещенск</span>
+                </div>
+            </a>
+        </swiper-slide>
+        <swiper-slide>
+            <a href="#" class="transition-all hover:opacity-95">
+                <div class="h-3/4 relative">
+                    <img class="h-full w-full object-cover" src="/images/car.jpg" alt="car">
+                    <span
+                        class="absolute bottom-4 left-4 bg-emerald-400 px-2 rounded-xl text-white text-sm">4 500 000 Р</span>
+                </div>
+                <div class="h-1/4 px-2 flex flex-col justify-center">
+                    <h4 class="text-xl mb-0.5">BMW X8</h4>
+                    <span class="text-md">Благовещенск</span>
+                </div>
+            </a>
+        </swiper-slide>
+        <swiper-slide>
+            <a href="#" class="transition-all hover:opacity-95">
+                <div class="h-3/4 relative">
+                    <img class="h-full w-full object-cover" src="/images/car.jpg" alt="car">
+                    <span
+                        class="absolute bottom-4 left-4 bg-emerald-400 px-2 rounded-xl text-white text-sm">4 500 000 Р</span>
+                </div>
+                <div class="h-1/4 px-2 flex flex-col justify-center">
+                    <h4 class="text-xl mb-0.5">BMW X8</h4>
+                    <span class="text-md">Благовещенск</span>
+                </div>
+            </a>
+        </swiper-slide>
+        <swiper-slide>
+            <a href="#" class="transition-all hover:opacity-95">
+                <div class="h-3/4 relative">
+                    <img class="h-full w-full object-cover" src="/images/car.jpg" alt="car">
+                    <span
+                        class="absolute bottom-4 left-4 bg-emerald-400 px-2 rounded-xl text-white text-sm">4 500 000 Р</span>
+                </div>
+                <div class="h-1/4 px-2 flex flex-col justify-center">
+                    <h4 class="text-xl mb-0.5">BMW X8</h4>
+                    <span class="text-md">Благовещенск</span>
+                </div>
+            </a>
+        </swiper-slide>
+    </swiper>
+</template>
+
+<style>
+.swiper-wrapper {
+    height: 300px;
+}
+
+.swiper-button-prev,
+.swiper-button-next {
+    color: rgba(52 211 153);
+    height: 100%;
+    width: 100px;
+    top: 0;
+    margin-top: 0;
+    opacity: 0;
+    transition: all .3s;
+}
+
+.swiper-button-prev {
+    left: -30px;
+}
+
+.swiper-button-next {
+    right: -30px;
+}
+
+.swiper:hover
+.swiper-button-next {
+    opacity: 1;
+    background: linear-gradient(to right, transparent, rgba(52, 211, 153, 0.3) 100%);
+    right: -15px;
+}
+
+.swiper:hover
+.swiper-button-prev {
+    opacity: 1;
+    left: -15px;
+    background: linear-gradient(to left, transparent, rgba(52, 211, 153, 0.3) 100%);
+}
+</style>
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,14 +1,7 @@
 <script setup>
 import Main from '@/Layouts/Main.vue';
 import {Head} from '@inertiajs/vue3';
-import {Swiper, SwiperSlide} from 'swiper/vue';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import {Navigation} from 'swiper/modules';
-import {ref} from "vue";
-
-const modules = ref([Navigation]);
-
+import Slider from "@/Components/Slider/Slider.vue";
 </script>
 
 <template>
@@ -18,26 +11,13 @@ const modules = ref([Navigation]);
         <div class="py-12">
             <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
                 <h1 class="text-emerald-400 text-4xl">Продажа авто в России</h1>
-                <div
-                    class="overflow-hidden bg-white shadow-sm sm:rounded-lg p-4 my-6"
-                >
-                    <swiper
-                        :slides-per-view="6"
-                        :space-between="50"
-                        :navigation="true"
-                        :modules="modules"
-                    >
-                        <swiper-slide>Slide 1</swiper-slide>
-                        <swiper-slide>Slide 2</swiper-slide>
-                        <swiper-slide>Slide 3</swiper-slide>
-                        <swiper-slide>Slide 3</swiper-slide>
-                        <swiper-slide>Slide 3</swiper-slide>
-                        <swiper-slide>Slide 3</swiper-slide>
-                        <swiper-slide>Slide 3</swiper-slide>
-                        <swiper-slide>Slide 3</swiper-slide>
-
-                    </swiper>
-                </div>
+            </div>
+            <div
+                class="overflow-hidden bg-white shadow-sm sm:rounded-lg my-6"
+            >
+                <Slider/>
+            </div>
+            <div>
             </div>
         </div>
     </Main>
