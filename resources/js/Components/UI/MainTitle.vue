8aa670604806859bf4ba9ac5d73d6f08f60fed8f
mlugovskoy <lugovskoy19@mail.ru>
2024-11-04 16:00:56 +0900

    refactor: Правки

---
+++ resources/js/Components/UI/MainTitle.vue
@@ -0,0 +1,16 @@
+<script setup>
+const props = defineProps({
+    href: {
+        type: String,
+        required: true,
+    }
+});
+</script>
+
+<template>
+    <h2 class="text-emerald-400 text-3xl sm:text-4xl mb-4 sm:mb-8 inline-block border-b-2 border-transparent transition-all hover:border-emerald-400">
+        <a :href="href">
+            <slot/>
+        </a>
+    </h2>
+</template>
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -4,6 +4,7 @@ import {Head} from '@inertiajs/vue3';
 import MainSlider from "@/Components/MainSlider/MainSlider.vue";
 import MainFilter from "@/Components/MainFilter/MainFilter.vue";
 import MainReviews from "@/Components/MainReviews/MainReviews.vue";
+import MainTitle from "@/Components/UI/MainTitle.vue";
 </script>
 
 <template>
@@ -12,11 +13,7 @@ import MainReviews from "@/Components/MainReviews/MainReviews.vue";
     <Main>
         <div class="py-6 sm:py-12">
             <div class="mx-auto max-w-7xl px-6 lg:px-8">
-                <h2 class="text-emerald-400 text-3xl sm:text-4xl mb-4 sm:mb-8 inline-block border-b-2 border-transparent transition-all hover:border-emerald-400">
-                    <a :href="route('home')">
-                        Продажа авто в России
-                    </a>
-                </h2>
+                <MainTitle :href="route('home')">Продажа авто в России</MainTitle>
             </div>
             <div
                 class="overflow-hidden bg-white shadow-sm sm:rounded-lg"
@@ -24,19 +21,10 @@ import MainReviews from "@/Components/MainReviews/MainReviews.vue";
                 <MainSlider/>
             </div>
             <div class="mx-auto max-w-7xl overflow-hidden bg-white shadow-sm mt-10 mb-14 p-6 lg:p-8 sm:rounded-lg">
-                <h2 class="text-emerald-400 text-3xl sm:text-4xl mb-4 sm:mb-8 inline-block border-b-2 border-transparent transition-all hover:border-emerald-400">
-                    <a :href="route('home')">
-                        Поиск объявлений
-                    </a>
-                </h2>
+                <MainTitle :href="route('home')">Поиск объявлений</MainTitle>
                 <MainFilter class="mb-20"/>
 
-                <h2 class="text-emerald-400 text-3xl sm:text-4xl mb-4 sm:mb-8 inline-block border-b-2 border-transparent transition-all hover:border-emerald-400">
-                    <a href="#">
-                        Отзывы автовладельцев
-                    </a>
-                </h2>
-
+                <MainTitle href="#">Отзывы автовладельцев</MainTitle>
                 <MainReviews/>
             </div>
         </div>
