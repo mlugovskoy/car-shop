cb0a3cfebb8cda061efdfc3a7a7df2266822367e
mlugovskoy <lugovskoy19@mail.ru>
2024-11-04 23:01:53 +0900

    feat: Добавил миграции для транспорта

---
+++ database/migrations/2024_11_04_124617_create_images_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->id();
+            $table->string('image_title', 255);
+            $table->string('image_path', 255);
+            $table->string('image_size', 255);
+            $table->string('image_ext', 255);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('images');
+    }
+};
