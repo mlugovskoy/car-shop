f539a085fc9c7eceac0721d1edf892be4b4524e9
mlugovskoy <lugovskoy19@mail.ru>
2024-11-04 20:05:35 +0900

    feat: Верстка блока Новости

---
+++ resources/js/Components/MainNews/MainNews.vue
@@ -0,0 +1,80 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+        <a href="#" class="rounded shadow group lg:col-span-2">
+            <img class="w-full h-[300px] object-cover rounded-t transition-all group-hover:opacity-85"
+                 src="/images/reviews/reviews-1.jpg" alt="X90 Plus">
+            <div class="rounded-b p-4">
+                <h4 class="text-xl mb-2">X90 Plus, 2024</h4>
+                <div class="mb-4">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias consectetur, in ipsam magni
+                    obcaecati quibusdam temporibus voluptate.
+                </div>
+                <div class="flex justify-between">
+                    <div class="flex gap-2">
+                        <svg class="fill-emerald-400" xmlns="http://www.w3.org/2000/svg" height="24px"
+                             viewBox="0 -960 960 960" width="24px">
+                            <path
+                                d="M80-80v-720q0-33 23.5-56.5T160-880h640q33 0 56.5 23.5T880-800v480q0 33-23.5 56.5T800-240H240L80-80Zm126-240h594v-480H160v525l46-45Zm-46 0v-480 480Z"/>
+                        </svg>
+                        <span>5</span>
+                    </div>
+                    <div>
+                        3 ноября
+                    </div>
+                </div>
+            </div>
+        </a>
+        <a href="#" class="rounded shadow group lg:col-span-2">
+            <img class="w-full h-[300px] object-cover rounded-t transition-all group-hover:opacity-85"
+                 src="/images/reviews/reviews-1.jpg" alt="X90 Plus">
+            <div class="rounded-b p-4">
+                <h4 class="text-xl mb-2">X90 Plus, 2024</h4>
+                <div class="mb-4">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias consectetur, in ipsam magni
+                    obcaecati quibusdam temporibus voluptate.
+                </div>
+                <div class="flex justify-between">
+                    <div class="flex gap-2">
+                        <svg class="fill-emerald-400" xmlns="http://www.w3.org/2000/svg" height="24px"
+                             viewBox="0 -960 960 960" width="24px">
+                            <path
+                                d="M80-80v-720q0-33 23.5-56.5T160-880h640q33 0 56.5 23.5T880-800v480q0 33-23.5 56.5T800-240H240L80-80Zm126-240h594v-480H160v525l46-45Zm-46 0v-480 480Z"/>
+                        </svg>
+                        <span>5</span>
+                    </div>
+                    <div>
+                        3 ноября
+                    </div>
+                </div>
+            </div>
+        </a>
+        <a href="#" class="rounded shadow group">
+            <img class="w-full h-[220px] object-cover rounded-t transition-all group-hover:opacity-85"
+                 src="/images/reviews/reviews-1.jpg" alt="X90 Plus">
+            <div class="rounded-b p-4">
+                <h4 class="text-xl mb-2">X90 Plus, 2024</h4>
+                <div class="mb-4">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias consectetur, in ipsam magni
+                    obcaecati quibusdam temporibus voluptate.
+                </div>
+                <div class="flex justify-between">
+                    <div class="flex gap-2">
+                        <svg class="fill-emerald-400" xmlns="http://www.w3.org/2000/svg" height="24px"
+                             viewBox="0 -960 960 960" width="24px">
+                            <path
+                                d="M80-80v-720q0-33 23.5-56.5T160-880h640q33 0 56.5 23.5T880-800v480q0 33-23.5 56.5T800-240H240L80-80Zm126-240h594v-480H160v525l46-45Zm-46 0v-480 480Z"/>
+                        </svg>
+                        <span>5</span>
+                    </div>
+                    <div>
+                        3 ноября
+                    </div>
+                </div>
+            </div>
+        </a>
+    </div>
+</template>
