ad2d30b505602a87a5547636faf0ecf9b36d752a
mlugovskoy <lugovskoy19@mail.ru>
2024-11-09 17:06:39 +0900

    feat: Добавил seeder для FuelType

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             ImageSeeder::class,
             MakerSeeder::class,
             ModelSeeder::class,
+            FuelTypeSeeder::class,
         ]);
     }
 }
