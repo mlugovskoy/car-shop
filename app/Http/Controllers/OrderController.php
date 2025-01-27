623bf9f2c090db9c5252885a1ad72405c441a1a7
mlugovskoy <lugovskoy19@mail.ru>
2025-01-27 23:30:57 +0900

    feat: Добавил страницу с историей заказов

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,13 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\OrderResource;
+use App\Repositories\OrderRepository;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class OrderController extends Controller
 {
+    protected OrderRepository $orderRepository;
+
+    public function __construct(OrderRepository $orderRepository)
+    {
+        $this->orderRepository = $orderRepository;
+    }
+
     public function index()
     {
-        return Inertia::render('Order/Index');
+        $orders = $this->orderRepository->getAllOrdersForCurrentUser();
+
+        $total = number_format($orders->sum('price'), 0, '.', ' ') . ' ₽';
+
+        return Inertia::render('Order/Index', ['items' => OrderResource::collection($orders), 'total' => $total]);
+    }
+
+    public function create()
+    {
+        return Inertia::render('Order/Create');
     }
 }
---
+++ resources/js/Pages/Order/Create.vue
@@ -0,0 +1,26 @@
+<script setup>
+import Main from '@/Layouts/Main.vue';
+import {Head, usePage} from '@inertiajs/vue3';
+import MainTitle from "@/Components/UI/MainTitle.vue";
+
+const page = usePage();
+</script>
+
+<template>
+    <Head title="Оформление заказа"/>
+
+    <Main>
+        <div class="mx-auto pt-12 max-w-7xl px-6 lg:px-8">
+            <MainTitle :href="route('order.create')">Оформление заказа</MainTitle>
+        </div>
+
+        <div class="flex gap-12 mx-auto max-w-7xl space-y-6 sm:px-6 lg:px-8">
+            <div class="bg-white w-4/6 p-4 shadow sm:rounded-lg sm:p-8">
+                test
+            </div>
+            <div class="bg-white w-2/6 p-4 shadow sm:rounded-lg sm:p-8">
+                Заказ
+            </div>
+        </div>
+    </Main>
+</template>
